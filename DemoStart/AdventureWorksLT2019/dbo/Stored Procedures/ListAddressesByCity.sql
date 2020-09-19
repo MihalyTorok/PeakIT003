9ad79a02e76ce4be8d96a90f61f59a7fd72a5146
Mihaly Torok <mihaly.torok@gmail.com>
2020-09-19 14:55:49 +0300

    Add stored procedure demo

---
+++ DemoStart/AdventureWorksLT2019/dbo/Stored Procedures/ListAddressesByCity.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[ListAddressesByCity]
+	@city NVARCHAR (30)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		[AddressID]  
+		,[AddressLine1]
+		,[AddressLine2]
+		,[City]        
+		,[StateProvince]
+		,[CountryRegion]
+		,[PostalCode]  
+		,[rowguid]     
+		,[ModifiedDate]
+	FROM SalesLT.[Address]
+	WHERE City = @city;
+
+END
+
