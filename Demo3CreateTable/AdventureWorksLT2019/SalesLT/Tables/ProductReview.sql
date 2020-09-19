822385361f6c55845fe216597db344e040e0751d
Mihaly Torok <mihaly.torok@gmail.com>
2020-09-19 14:23:37 +0300

    Add the ProductReview table

---
+++ Demo3CreateTable/AdventureWorksLT2019/SalesLT/Tables/ProductReview.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProductReview]
+(
+	[ProductReviewID] INT IDENTITY NOT NULL, 
+    [ProductID] INT NOT NULL, 
+    [Message] NVARCHAR(255) NULL, 
+    [Rating] INT NOT NULL, 
+    CONSTRAINT [PK_ProductReview] PRIMARY KEY ([ProductReviewID]), 
+    CONSTRAINT [FK_ProductReview_Product] FOREIGN KEY ([ProductID]) REFERENCES SalesLT.Product([ProductID]), 
+    CONSTRAINT [CK_ProductReview_Rating] CHECK (Rating BETWEEN 1 AND 5)
+)
